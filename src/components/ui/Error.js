28725a22014d87095f17f961a68c4e4a51b8a744
emon <imrannaaziremon@gmail.com>
2023-03-15 08:07:50 +0600

    videos fetched and rendered in ui

--- src/components/ui/Error.js
+++ src/components/ui/Error.js
@@ -1,7 +1,9 @@
-export default function Error() {
+export default function Error({ children }) {
     return (
         <div className="w-full flex items-center justify-center h-10 max-w-7xl mx-auto p-2 text-red-700 bg-red-100 col-span-12">
-            There was an error occured!
+            {
+                children
+            }
         </div>
     );
 }
--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,26 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
+    const { data: videos, isLoading, isError, error } = useGetVideosQuery()
+
+    // decide what to render
+    let content;
+    if (isLoading) content = (<>
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+    </>)
+    if (!isLoading && isError) content = <Error>{error?.error}</Error>
+    if (!isLoading && !isError) content = <Error>No video found!</Error>
+    if (!isLoading && !isError && videos.length > 0) content = videos.map((video) => <Video key={video.id} video={video} />)
     return (
         <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
+            {content}
         </>
     );
 }
